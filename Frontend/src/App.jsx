710b0f4a91e271fb17be40eeff837a650782615a
FrankAlessandro <frank.alessandro.roldan@gmail.com>
2023-11-17 12:36:11 -0500

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { AsesorView } from "./Components/AsesorView/AsesorView"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <AsesorView/>
+    </>
+  )
+}
+
+export default App
