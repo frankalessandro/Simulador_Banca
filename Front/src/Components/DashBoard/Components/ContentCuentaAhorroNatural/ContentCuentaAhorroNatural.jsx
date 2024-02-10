3a8267ee0462fb0ffe813908fec82289b150faa6
Frank Alessandro <frank.alessandro.roldan@gmail.com>
2024-02-10 14:11:49 -0500

    Cofig react router dom

--- Front/src/Components/DashBoard/Components/ContentCuentaAhorroNatural/ContentCuentaAhorroNatural.jsx
+++ Front/src/Components/DashBoard/Components/ContentCuentaAhorroNatural/ContentCuentaAhorroNatural.jsx
@@ -3,13 +3,26 @@ import React from 'react'
 export const ContentCuentaAhorroNatural = () => {
   return (
     <>
-    <div class="p-4 sm:ml-64">
-                <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-                    <div className='ContentFormNaturalPerson' class='bg-green-200 h-lvh' style={{ minHeight: '85vh' }}>
-                        <h1>Jimmi Falllon</h1>
-                    </div>
+      <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+          <div className='ContentFormNaturalPerson' class='bg-green-200 h-lvh' style={{ minHeight: '85vh' }}>
+
+            <form>
+              <div class="grid gap-6 mb-6 md:grid-cols-2">
+                <div>
+                  <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                  <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required/>
                 </div>
-            </div>
+                <div>
+                  <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+                  <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required/>
+                </div>
+                </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
