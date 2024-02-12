b5bf8c3be31eeb4700d1debde64c03f0a39e19cd
WilliamB0517 <willischurro16@gmail.com>
2024-02-12 13:44:47 -0500

    Landing2.0

--- Front/src/Components/LandingPage/Carousel/Carousel.jsx
+++ Front/src/Components/LandingPage/Carousel/Carousel.jsx
@@ -1,39 +1,42 @@
 import React from 'react'
 import FrontImage from '../../../assets/Img/Main_Image.svg'
-import prueba from '../../../assets/Img/prueba1.jpg'
+import prueba from '../../../assets/Img/prueba2.jpg'
+import prueba1 from '../../../assets/Img/carousel1.png'
+import prueba2 from '../../../assets/Img/carousel2.png'
+import prueba3 from '../../../assets/Img/carousel3.png'
 import './Carousel.css'
 export const Carousel = () => {
     return (
         <>
-            <div className="p-3">
+            <div className="p-3" class="flex place-content-center">
 
-                <div id="controls-carousel" class="relative w-full" data-carousel="static">
+                <div id="controls-carousel" class="relative w-9/12 mt-2  " data-carousel="static">
                
                     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                      
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src={prueba} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                            <img src={prueba} class="absolute block w-4/5 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                         </div>
                      
                         <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                            <img src={FrontImage} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                            <img src={FrontImage} class="absolute block w-6/12 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                         </div>
           
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-3.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                            <img src={prueba1} class="absolute block w-6/12 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                         </div>
                  
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-4.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                            <img src={prueba2} class="absolute block w-6/12 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                         </div>
 
                         <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                            <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
+                            <img src={prueba3} class="absolute block w-9/12 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
                         </div>
                     </div>
 
                     <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-green/30 dark:bg-gray-800/30 group-hover:bg-green/40 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
                             </svg>
@@ -41,7 +44,7 @@ export const Carousel = () => {
                         </span>
                     </button>
                     <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-green/30 dark:bg-gray-800/30 group-hover:bg-green/40 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                             </svg>
