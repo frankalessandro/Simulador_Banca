7967a10c609e3357efe8186039bba106373cd489
Frank Alessandro <frank.alessandro.roldan@gmail.com>
2024-02-15 13:19:02 -0500

    Correción de errores visuales

--- Front/src/Components/DashBoard/DashboardComponent.jsx
+++ Front/src/Components/DashBoard/DashboardComponent.jsx
@@ -5,7 +5,7 @@ import { ContentCuentaAhorroNatural } from './Components/ContentCuentaAhorroNatu
 import { PrincipalPage } from './Components/PrincipalPage'
 export const DashboardComponent = () => {
 
-    const [contenidoSeleccionado, setContenidoSeleccionado] = useState(1);
+    const [contenidoSeleccionado, setContenidoSeleccionado] = useState('PrincipalPage');
     // Función para manejar clics de botones
     const handleBotonClick = (contenido) => {
         setContenidoSeleccionado(contenido);
@@ -188,14 +188,7 @@ export const DashboardComponent = () => {
                                 <span class="flex-1 ms-3 whitespace-nowrap">Inversiones</span>
                             </a>
                         </li> */}
-                        <li>
-                            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-                                </svg>
-                                <span class="flex-1 ms-3 whitespace-nowrap">Salir</span>
-                            </a>
-                        </li>
+                        
                     </ul>
                 </div>
             </aside>
