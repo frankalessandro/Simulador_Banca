229c4e3ddd5c82e8cdc5b9e6b344b2c7a869eb45
Frank Alessandro <frank.alessandro.roldan@gmail.com>
2024-02-15 13:51:27 -0500

    no disponible

--- Front/src/Components/DashBoard/Components/PrincipalPage.jsx
+++ Front/src/Components/DashBoard/Components/PrincipalPage.jsx
@@ -3,12 +3,12 @@ import PrincipalImage from '../../../assets/Img/Login/Inicio2.png'
 export const PrincipalPage = () => {
   return (
     <div class="p-4 sm:ml-64">
-    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
         <div class='bg-green-200 flex justify-center items-center flex-col gap-24' style={{ minHeight: '85vh' }}>
-            <p class='font-bold text-5xl'>Bienvenido, Asesor</p>
-            <img class='w-1/3' src={PrincipalImage} alt="" />
+          <p class='font-bold text-5xl'>Bienvenido, Asesor</p>
+          <img class='w-1/3' src={PrincipalImage} alt="" />
         </div>
+      </div>
     </div>
-</div>
   )
 }
