becc0f45f30bb11ee966267f733a2af35959d82c
jeni84 <willischurro16@gmail.com>
2024-02-16 12:42:30 -0500

    3.0

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,6 +3,7 @@ import { Home } from "./Home/Home"
 import { DashboardMenu } from "./Pages/DashboardMenu";
 import { Login } from './Components/Login/Login';
 import { LandingPage } from './Pages/LandingPage';
+
 export default function MyPage() {
    return (
       <div>
