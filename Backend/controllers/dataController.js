e34567730af82c400da9c9c10fc1d3e57e3a00c3
YuranyRivera <160735735+YuranyRivera@users.noreply.github.com>
2024-03-13 15:12:47 -0500

    Feat: eliminar usuario

--- Backend/controllers/dataController.js
+++ Backend/controllers/dataController.js
@@ -52,7 +52,7 @@ const loginUser = async (req, res) => {
 const user = async (req, res) => {
   try {
 
-    const result = await pool.query('SELECT * FROM usuarios')
+    const result = await pool.query('SELECT * FROM usuarios ' )
 
     if (result.rows.length > 0) {
       return res.status(200).json({ result });
@@ -273,8 +273,35 @@ const Estado = async (req, res) => {
   }
 };
 
+const DelateUser = async (req , res ) => {
+  try {
+    const userId = req.params.userId;
+
+    const result2 = await pool.query(`
+    UPDATE DetalleProducto
+    SET responsable = NULL
+    WHERE responsable = ${userId}
+    `) 
+
+    // Buscar el usuario por su ID
+  const result = await pool.query(
+    
+    `DELETE  FROM public.usuarios
+    WHERE ID_Usuario = ${userId};`
+  ) 
+
+    // Enviar una respuesta de éxito
+    res.status(200).json({ message: 'Usuario eliminado correctamente' });
+} catch (error) {
+    // Si ocurre algún error, enviar una respuesta de error con el mensaje
+    console.error('Error al eliminar usuario:', error);
+    res.status(500).json({ message: 'Error al eliminar usuario' });
+}
+}
+
 module.exports = {
   loginUser,
+  DelateUser,
   user,
   getPendiente,
   getAutorizado,
